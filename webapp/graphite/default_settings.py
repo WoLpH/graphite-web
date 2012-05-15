ebf225cd81c165917c2c0b0982c3d7d7062d6f89
WoLpH <Rick@Fawo.nl>
2012-05-15 15:16:53 +0000

    pep8ified

--- webapp/graphite/default_settings.py
+++ webapp/graphite/default_settings.py
@@ -1,4 +1,8 @@
-"""Copyright 2008 Orbitz WorldWide
+# Django settings for graphite project.
+# DO NOT MODIFY THIS FILE DIRECTLY - use local_settings.py instead
+# Import all defaults from Django so we will never break imports in
+# local_settings
+'''Copyright 2008 Orbitz WorldWide
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -10,12 +14,8 @@ Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
-limitations under the License."""
-# Django settings for graphite project.
-# DO NOT MODIFY THIS FILE DIRECTLY - use local_settings.py instead
+limitations under the License.'''
 
-# Import all defaults from Django so we will never break imports in
-# local_settings
 from django.conf.global_settings import *
 
 import os
@@ -23,9 +23,9 @@ import sys
 from django import VERSION as DJANGO_VERSION
 
 try:
-  import rrdtool
+    import rrdtool
 except ImportError:
-  rrdtool = False
+    rrdtool = False
 
 WEBAPP_VERSION = '0.9.10-pre1'
 DEBUG = False
@@ -36,6 +36,7 @@ WEB_DIR = os.path.dirname(os.path.abspath(__file__))
 WEBAPP_DIR = os.path.dirname(WEB_DIR)
 GRAPHITE_ROOT = os.path.dirname(WEBAPP_DIR)
 THIRDPARTY_DIR = os.path.join(WEB_DIR, 'thirdparty')
+
 # Initialize additional path variables
 # Defaults for these are set after local_settings is imported
 CONTENT_DIR = ''
@@ -54,12 +55,14 @@ DATA_DIRS = []
 CLUSTER_SERVERS = []
 
 sys.path.insert(0, WEBAPP_DIR)
+
 # Allow local versions of the libs shipped in thirdparty to take precedence
 sys.path.append(THIRDPARTY_DIR)
 
 # Memcache settings
 MEMCACHE_HOSTS = []
-DEFAULT_CACHE_DURATION = 60 #metric data and graphs are cached for one minute by default
+# metric data and graphs are cached for one minute by default
+DEFAULT_CACHE_DURATION = 60
 LOG_CACHE_PERFORMANCE = False
 
 # Remote store settings
@@ -68,62 +71,62 @@ REMOTE_STORE_FIND_TIMEOUT = 2.5
 REMOTE_STORE_RETRY_DELAY = 60
 REMOTE_FIND_CACHE_DURATION = 300
 
-#Remote rendering settings
-REMOTE_RENDERING = False #if True, rendering is delegated to RENDERING_HOSTS
+# Remote rendering settings
+REMOTE_RENDERING = False  # if True, rendering is delegated to RENDERING_HOSTS
 RENDERING_HOSTS = []
 REMOTE_RENDER_CONNECT_TIMEOUT = 1.0
 LOG_RENDERING_PERFORMANCE = False
 
-#Miscellaneous settings
-CARBONLINK_HOSTS = ["127.0.0.1:7002"]
+# Miscellaneous settings
+CARBONLINK_HOSTS = ['127.0.0.1:7002']
 CARBONLINK_TIMEOUT = 1.0
-SMTP_SERVER = "localhost"
-DOCUMENTATION_URL = "http://graphite.readthedocs.org/"
+SMTP_SERVER = 'localhost'
+DOCUMENTATION_URL = 'http://graphite.readthedocs.org/'
 ALLOW_ANONYMOUS_CLI = True
 LOG_METRIC_ACCESS = False
 LEGEND_MAX_ITEMS = 10
 
-#Authentication settings
+# Authentication settings
 USE_LDAP_AUTH = False
-LDAP_SERVER = "" # "ldapserver.mydomain.com"
+LDAP_SERVER = ''  # "ldapserver.mydomain.com"
 LDAP_PORT = 389
-LDAP_SEARCH_BASE = "" # "OU=users,DC=mydomain,DC=com"
-LDAP_BASE_USER = "" # "CN=some_readonly_account,DC=mydomain,DC=com"
-LDAP_BASE_PASS = "" # "my_password"
-LDAP_USER_QUERY = "" # "(username=%s)"  For Active Directory use "(sAMAccountName=%s)"
+LDAP_SEARCH_BASE = ''  # "OU=users,DC=mydomain,DC=com"
+LDAP_BASE_USER = ''  # "CN=some_readonly_account,DC=mydomain,DC=com"
+LDAP_BASE_PASS = ''  # "my_password"
+# "(username=%s)"  For Active Directory use "(sAMAccountName=%s)"
+LDAP_USER_QUERY = ''
 LDAP_URI = None
 
-#Set this to True to delegate authentication to the web server
+# Set this to True to delegate authentication to the web server
 USE_REMOTE_USER_AUTHENTICATION = False
 
 # Override to link a different URL for login (e.g. for django_openid_auth)
 LOGIN_URL = '/account/login'
 
-#Additional authentication backends to prepend
+# Additional authentication backends to prepend
 ADDITIONAL_AUTHENTICATION_BACKENDS = []
 
-#Initialize database settings - Old style (pre 1.2)
-DATABASE_ENGINE = 'django.db.backends.sqlite3'	# 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
-DATABASE_NAME = ''				# Or path to database file if using sqlite3.
-DATABASE_USER = ''				# Not used with sqlite3.
-DATABASE_PASSWORD = ''				# Not used with sqlite3.
-DATABASE_HOST = ''				# Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''				# Set to empty string for default. Not used with sqlite3.
+# Initialize database settings - Old style (pre 1.2)
+# 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
+DATABASE_ENGINE = 'django.db.backends.sqlite3'
+DATABASE_NAME = ''  # Or path to database file if using sqlite3.
+DATABASE_USER = ''  # Not used with sqlite3.
+DATABASE_PASSWORD = ''  # Not used with sqlite3.
+DATABASE_HOST = ''  # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = ''  # Set to empty string for default. Not used with sqlite3.
 
 ADMINS = ()
 MANAGERS = ADMINS
 
-TEMPLATE_DIRS = (
-  os.path.join(WEB_DIR, 'templates'),
-)
+TEMPLATE_DIRS = (os.path.join(WEB_DIR, 'templates'), )
 
 # If using rrdcached, set to the address or socket of the daemon
 FLUSHRRDCACHED = ''
 
-#Django settings below, do not touch!
+# Django settings below, do not touch!
 APPEND_SLASH = False
 TEMPLATE_DEBUG = DEBUG
-CACHE_BACKEND = "dummy:///"
+CACHE_BACKEND = 'dummy:///'
 
 # Language code for this installation. All choices can be found here:
 # http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
@@ -133,7 +136,6 @@ LANGUAGE_CODE = 'en-us'
 SITE_ID = 1
 
 # Absolute path to the directory that holds media.
-
 MEDIA_ROOT = ''
 
 # URL that handles the media served from MEDIA_ROOT.
@@ -149,43 +151,39 @@ ADMIN_MEDIA_PREFIX = '/media/'
 SECRET_KEY = ''
 
 # List of callables that know how to import templates from various sources.
-#XXX Compatibility for Django 1.1. To be removed after 0.9.10
-if DJANGO_VERSION < (1,2):
-  TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
-  )
+# XXX Compatibility for Django 1.1. To be removed after 0.9.10
+if DJANGO_VERSION < (1, 2):
+    TEMPLATE_LOADERS = \
+        ('django.template.loaders.filesystem.load_template_source',
+         'django.template.loaders.app_directories.load_template_source')
 else:
-  TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-  )
+    TEMPLATE_LOADERS = ('django.template.loaders.filesystem.Loader',
+                        'django.template.loaders.app_directories.Loader')
 
-MIDDLEWARE_CLASSES = (
-  'django.middleware.common.CommonMiddleware',
-  'django.middleware.gzip.GZipMiddleware',
-  'django.contrib.sessions.middleware.SessionMiddleware',
-  'django.contrib.auth.middleware.AuthenticationMiddleware',
-)
+MIDDLEWARE_CLASSES = ('django.middleware.common.CommonMiddleware',
+                      'django.middleware.gzip.GZipMiddleware',
+                      'django.contrib.sessions.middleware.SessionMiddleware',
+                      'django.contrib.auth.middleware.AuthenticationMiddlewar'
+                      'e')
 
 ROOT_URLCONF = 'graphite.urls'
 
 INSTALLED_APPS = (
-  'graphite.metrics',
-  'graphite.render',
-  'graphite.cli',
-  'graphite.browser',
-  'graphite.composer',
-  'graphite.account',
-  'graphite.dashboard',
-  'graphite.whitelist',
-  'graphite.events',
-  'django.contrib.auth',
-  'django.contrib.sessions',
-  'django.contrib.admin',
-  'django.contrib.contenttypes',
-  'tagging',
-)
+    'graphite.metrics',
+    'graphite.render',
+    'graphite.cli',
+    'graphite.browser',
+    'graphite.composer',
+    'graphite.account',
+    'graphite.dashboard',
+    'graphite.whitelist',
+    'graphite.events',
+    'django.contrib.auth',
+    'django.contrib.sessions',
+    'django.contrib.admin',
+    'django.contrib.contenttypes',
+    'tagging',
+    )
 
 AUTHENTICATION_BACKENDS = ['django.contrib.auth.backends.ModelBackend']
 
--- webapp/graphite/settings.py
+++ webapp/graphite/settings.py
@@ -3,69 +3,87 @@
 from __future__ import absolute_import
 import os
 
-## Get defaults from default_settings
+# Get defaults from default_settings
 from default_settings import *
 
-## Pull in overrides from local_settings.py
+# Pull in overrides from local_settings.py
 try:
-  from local_settings import *
+    from local_settings import *
 except ImportError:
-  print >> sys.stderr, "Could not import graphite.local_settings, using defaults!"
+    print >> sys.stderr, ('Could not import graphite.local_settings, '
+        'using defaults!')
 
-
-## Set config dependent on flags set in local_settings
+# Set config dependent on flags set in local_settings
 # Path configuration
 if not CONTENT_DIR:
-  CONTENT_DIR = os.path.join(WEBAPP_DIR, 'content')
+    CONTENT_DIR = os.path.join(WEBAPP_DIR, 'content')
+
 if not CSS_DIR:
-  CSS_DIR = os.path.join(CONTENT_DIR, 'css')
+    CSS_DIR = os.path.join(CONTENT_DIR, 'css')
 
 if not CONF_DIR:
-  CONF_DIR = os.environ.get('GRAPHITE_CONF_DIR', os.path.join(GRAPHITE_ROOT, 'conf'))
+    CONF_DIR = os.environ.get(
+        'GRAPHITE_CONF_DIR',
+        os.path.join(GRAPHITE_ROOT, 'conf'),
+    )
+
 if not DASHBOARD_CONF:
-  DASHBOARD_CONF = os.path.join(CONF_DIR, 'dashboard.conf')
+    DASHBOARD_CONF = os.path.join(CONF_DIR, 'dashboard.conf')
+
 if not GRAPHTEMPLATES_CONF:
-  GRAPHTEMPLATES_CONF = os.path.join(CONF_DIR, 'graphTemplates.conf')
+    GRAPHTEMPLATES_CONF = os.path.join(CONF_DIR, 'graphTemplates.conf')
 
 if not STORAGE_DIR:
-  STORAGE_DIR = os.environ.get('GRAPHITE_STORAGE_DIR', os.path.join(GRAPHITE_ROOT, 'storage'))
-if not WHITELIST_FILE:
-  WHITELIST_FILE = os.path.join(STORAGE_DIR, 'lists', 'whitelist')
-if not INDEX_FILE:
-  INDEX_FILE = os.path.join(STORAGE_DIR, 'index')
-if not LOG_DIR:
-  LOG_DIR = os.path.join(STORAGE_DIR, 'log', 'webapp')
-if not WHISPER_DIR:
-  WHISPER_DIR = os.path.join(STORAGE_DIR, 'whisper/')
-if not RRD_DIR:
-  RRD_DIR = os.path.join(STORAGE_DIR, 'rrd/')
-if not DATA_DIRS:
-  if rrdtool and os.path.exists(RRD_DIR):
-    DATA_DIRS = [WHISPER_DIR, RRD_DIR]
-  else:
-    DATA_DIRS = [WHISPER_DIR]
+    STORAGE_DIR = os.environ.get(
+        'GRAPHITE_STORAGE_DIR',
+        os.path.join(GRAPHITE_ROOT, 'storage'),
+    )
 
+if not WHITELIST_FILE:
+    WHITELIST_FILE = os.path.join(STORAGE_DIR, 'lists', 'whitelist')
+
+if not INDEX_FILE:
+    INDEX_FILE = os.path.join(STORAGE_DIR, 'index')
+
+if not LOG_DIR:
+    LOG_DIR = os.path.join(STORAGE_DIR, 'log', 'webapp')
+
+if not WHISPER_DIR:
+    WHISPER_DIR = os.path.join(STORAGE_DIR, 'whisper/')
+
+if not RRD_DIR:
+    RRD_DIR = os.path.join(STORAGE_DIR, 'rrd/')
+
+if not DATA_DIRS:
+    if rrdtool and os.path.exists(RRD_DIR):
+        DATA_DIRS = [WHISPER_DIR, RRD_DIR]
+    else:
+        DATA_DIRS = [WHISPER_DIR]
 # Default sqlite db file
 # This is set here so that a user-set STORAGE_DIR is available
+
 if 'sqlite3' in DATABASE_ENGINE \
-    and not DATABASE_NAME:
-  DATABASE_NAME = os.path.join(STORAGE_DIR, 'graphite.db')
-
+        and not DATABASE_NAME:
+    DATABASE_NAME = os.path.join(STORAGE_DIR, 'graphite.db')
 # Caching shortcuts
-if MEMCACHE_HOSTS:
-  CACHE_BACKEND = 'memcached://%s/?timeout=%d' % (
-    ';'.join(MEMCACHE_HOSTS),
-    DEFAULT_CACHE_DURATION,
-  )
 
+if MEMCACHE_HOSTS:
+    CACHE_BACKEND = 'memcached://%s/?timeout=%d' % (
+        ';'.join(MEMCACHE_HOSTS),
+        DEFAULT_CACHE_DURATION,
+    )
 # Authentication shortcuts
+
 if USE_LDAP_AUTH and LDAP_URI is None:
-  LDAP_URI = "ldap://%s:%d/" % (LDAP_SERVER, LDAP_PORT)
+    LDAP_URI = 'ldap://%s:%d/' % (LDAP_SERVER, LDAP_PORT)
 
 if USE_REMOTE_USER_AUTHENTICATION:
-  MIDDLEWARE_CLASSES += ('django.contrib.auth.middleware.RemoteUserMiddleware',)
-  AUTHENTICATION_BACKENDS.insert(0,'django.contrib.auth.backends.RemoteUserBackend')
+    MIDDLEWARE_CLASSES += (
+        'django.contrib.auth.middleware.RemoteUserMiddleware',)
+    AUTHENTICATION_BACKENDS.insert(0,
+        'django.contrib.auth.backends.RemoteUserBackend')
 
 if USE_LDAP_AUTH:
-  AUTHENTICATION_BACKENDS.insert(0,'graphite.account.ldapBackend.LDAPBackend')
+    AUTHENTICATION_BACKENDS.insert(0,
+        'graphite.account.ldapBackend.LDAPBackend')
 
