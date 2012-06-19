f30c9e0b63d74d9a2dc3bed70f7c55e4b034bf18
WoLpH <Rick@Fawo.nl>
2012-06-19 12:32:08 +0000

    added basic autoindenting support

--- webapp/content/js/composer_widgets.js
+++ webapp/content/js/composer_widgets.js
@@ -967,6 +967,8 @@ function createFunctionsMenu() {
     }, {
       text: 'Filter',
       menu: [
+        {text: 'top', handler: applyFuncToEachWithInput('limit', 'Limit to the top ___ of a list of metrics')},
+        {text: 'bottom', handler: applyFuncToEachWithInput('limit', 'Limit to the top ___ of a list of metrics')},
         {
           text: 'Data Filters',
           menu: [
